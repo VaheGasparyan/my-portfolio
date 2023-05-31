961a1aecee6fc6cbbd76409887e723655174dc63
VaheGasparyan <vahegsparyan@gmail.com>
2023-05-31 17:10:03 +0400

    add my new project

--- src/services/projects_data.ts
+++ src/services/projects_data.ts
@@ -3,6 +3,7 @@ import { v4 as uuid } from 'uuid';
 import countries from 'images/countries.png';
 import todoList from 'images/todo-list.png';
 import memoryGame from 'images/memory-game.png';
+import ageCalculator from 'images/age-calculator.png';
 
 export const projects_data = [
     {
@@ -27,5 +28,13 @@ export const projects_data = [
         id: uuid(),
         tools: ['HTML, ', 'Css, ', 'Javascript'],
         projectLink: 'https://memory-game-tan-phi.vercel.app/'
+    },
+
+    {
+        imgSrc: ageCalculator,
+        name: 'Age Calculator',
+        id: uuid(),
+        tools: ['Material UI, ', 'Css, ', 'React js', 'Typescript'],
+        projectLink: 'https://age-calculator-black.vercel.app/'
     }
 ]
