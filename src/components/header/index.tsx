3059383130383dff61a1867bf8cb8c67fd259c48
VaheGasparyan <vahegsparyan@gmail.com>
2023-05-31 16:12:47 +0400

    example ok

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -20,7 +20,7 @@ const Header = () => {
 
             <nav className={closeIcon ? 'navbar active' : 'navbar'}>
                 <a href="#home" className='active headerHome' onClick={handleClick}>Home</a>
-                <a href="#about" className='headerAbout' onClick={handleClick}>Aboutttttttttt</a>
+                <a href="#about" className='headerAbout' onClick={handleClick}>About</a>
                 <a href="#skills" className='headerSkills' onClick={handleClick}>Skills</a>
                 <a href="#projects" className='headerProjects' onClick={handleClick}>Projects</a>
             </nav>
