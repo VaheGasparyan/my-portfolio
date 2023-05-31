03bb4440f843e06298dda080e6cafb075783ebae
VaheGasparyan <vahegsparyan@gmail.com>
2023-05-31 16:08:25 +0400

    fix header navbars animation

--- src/utils/checkScrollOffset.ts
+++ src/utils/checkScrollOffset.ts
@@ -8,10 +8,10 @@ export const checkScrollOffset = (home: HTMLDivElement, about: HTMLDivElement, s
     window.addEventListener('scroll', () => {
         const top = window.scrollY;
 
-        homeHeader!.classList.toggle('active', top >= home.offsetTop - 150 && top < home.offsetHeight + home.offsetTop);
-        aboutHeader!.classList.toggle('active', top >= about.offsetTop - 150 && top < about.offsetHeight + about.offsetTop);
-        skillsHeader!.classList.toggle('active', top >= skills.offsetTop - 150 && top < skills.offsetHeight + skills.offsetTop);
-        projectsHeader!.classList.toggle('active', top >= projects.offsetTop - 150 && top < projects.offsetHeight + projects.offsetTop);
+        homeHeader!.classList.toggle('active', top >= home.offsetTop - 150 && top < home.offsetHeight + home.offsetTop - 120);
+        aboutHeader!.classList.toggle('active', top >= about.offsetTop - 150 && top < about.offsetHeight + about.offsetTop - 125);
+        skillsHeader!.classList.toggle('active', top >= skills.offsetTop - 150 && top < skills.offsetHeight + skills.offsetTop - 120);
+        projectsHeader!.classList.toggle('active', top >= projects.offsetTop - 150 && top < projects.offsetHeight + projects.offsetTop - 120);
         header!.classList.toggle('sticky', top > 100);
     })
 }
